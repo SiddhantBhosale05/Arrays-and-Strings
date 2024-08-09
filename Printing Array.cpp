6dc16a4b674a6a0fc54800800b76c555234ff1e5
SiddhantBhosale05 <siddhant.bhosale05@gmail.com>
2024-08-09 11:25:10 +0530

    Add files via upload

---
+++ Printing Array.cpp
@@ -0,0 +1,26 @@
+//Name:Siddhant Kiran Bhosale
+//PRN:23070123038
+//Aim:Implemention of Arrays and Strings to print the given Array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int x[6] = {19,10,8,6,8};
+ for(int i=0;i<5;i++)
+ {
+    cout << x[i];
+    cout<< endl;
+ }
+
+    return 0;
+}
+
+
+/*Output:
+19
+10
+8
+6
+8*/
