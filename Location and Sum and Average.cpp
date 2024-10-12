e687d4d4404dff62f2273a07e263cd9a11025d5a
Siddhant Bhosale <siddhant.bhosale05@gmail.com>
2024-10-12 16:12:21 +0530

    Location and Sum and Average.cpp

--- Location and Sum and Average.cpp
+++ Location and Sum and Average.cpp
@@ -1,6 +1,6 @@
 //Name:Siddhant Kiran Bhosale
 //PRN:23070123038
-//Aim:Implemention of Arrays and Strings to print the Keys Location and Sum and Average of the Array
+//Aim:Implementation of Arrays and Strings to print the Keys Location and Sum and Average of the Array
 
 #include<iostream>
 using namespace std;
